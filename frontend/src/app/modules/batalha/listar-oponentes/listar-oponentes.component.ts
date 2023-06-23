abe465e524478f78f622b3c15e40af273b2ad5f3
akaMiike <mike.nss@alu.ufc.br>
2023-06-23 17:29:43 -0300

    feat: added initial players card layout

--- frontend/src/app/modules/batalha/listar-oponentes/listar-oponentes.component.ts
+++ frontend/src/app/modules/batalha/listar-oponentes/listar-oponentes.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarOponentesComponent implements OnInit {
 
+  nome = 'Jogador 1';
+
   constructor() { }
 
   ngOnInit(): void {
